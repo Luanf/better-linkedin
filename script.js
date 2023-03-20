faddf0ce374182bef59188c8d38b28a1231d169e
Luan Ferreira <luan.ferreira@clevertech.biz>
2023-03-20 15:55:18 -0300

    the basic idea

---
+++ script.js
@@ -0,0 +1,10 @@
+window.onload = function () {
+  if (document.getElementById("main")) {
+    document.getElementById("main").remove()
+  }
+}
+window.onscroll = function () {
+  if (document.getElementById("main")) {
+    document.getElementById("main").remove()
+  }
+}
